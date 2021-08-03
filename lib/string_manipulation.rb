060d9097ba252d966a513945e90a249cc32b00ae
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-08-03 12:23:31 -0300

    Add method to generate the histogram (for reuse) and now the code throws exceptions if given the wrong type of input.

--- lib/string_manipulation.rb
+++ lib/string_manipulation.rb
@@ -1,20 +1,24 @@
 class StringManipulation
+  #Generate histogram (Hash) with number of ocurrences of each character (char => ocurrences).
+  def generate_histogram(arr_chars)
+    if arr_chars.instance_of?(Array) then 
+      histogram = Hash.new(0)
+      arr_chars.each { |char| histogram[char] += 1 unless char == " " }
+      return histogram
+    else 
+      raise TypeError, "Expected Array but was given #{arr_chars.class}.", caller
+    end
+  end
 
   #Counts how many characters were repeated in a string, except for whitespaces.
   def count_characters(str)
-    counter = 0
-    histogram = Hash.new(0)
-    arr_chars = str.chars
-
-    arr_chars.each { |char|
-      histogram[char] += 1 unless char == " "
-    }
-
-    histogram.each_value { |occurence|
-      counter += 1 if occurence > 1
-    }
-
-    return counter
-  end
-  
+    if str.instance_of?(String) then
+      counter = 0
+      histogram = generate_histogram(str.chars)
+      histogram.each_value { |occurence| counter += 1 if occurence > 1 }
+      return counter
+    else
+      raise TypeError, "Expected String but was given #{str.class}.", caller
+    end
+  end 
 end
--- spec/string_manipulation_spec.rb
+++ spec/string_manipulation_spec.rb
@@ -1,21 +1,23 @@
 require 'string_manipulation'
 
 describe StringManipulation do
+  let(:subject) { StringManipulation.new }
   let(:hello) { "Hello!" }
   let(:hello_world) { "Hello World!" }
   let(:terminator) { "I'll be back..." }
+  let(:array_input) { [1,2,3] }
+
+  context "When not given a string" do
+    it "should raise a TypeError telling what type was given." do
+      expect { subject.count_characters(array_input) }.to raise_error(TypeError, "Expected String but was given Array.")
+    end
+  end
 
   context "When given a string" do
-    before do
-      subject = StringManipulation.new
-    end
-
     it "should count the number of duplicate characters." do
       expect(subject.count_characters(hello)).to eq(1)
       expect(subject.count_characters(hello_world)).to eq(2)
       expect(subject.count_characters(terminator)).to eq(3)
     end
-
-    
   end
 end
